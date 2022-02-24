4ca692498b74aeb9d3f6e0b4e03d8a85d920249b
Alison Aguiar <alison@gclaims.com.br>
2022-02-24 11:08:11 -0300

    add response application/importmap+json

--- src/index.js
+++ src/index.js
@@ -7,7 +7,21 @@ const app = express();
 app.use(cors());
 
 app.get('/', (req, res) => {
-  res.send('Hello World!');
+  res.setHeader('content-type', 'application/importmap+json');
+
+  res.send(JSON.stringify({
+    "imports": {
+      "@poc/root-config": "//localhost:9000/poc-root-config.js",
+      "@poc/angular-app1": "//localhost:9001/main.js",
+      "@poc/angular-nav": "//localhost:9002/main.js",
+      "@poc/angular-acessos": "//localhost:9003/main.js",
+      "@poc/angular-footer": "//localhost:9004/main.js",
+      "@poc/react-app1": "//localhost:8500/poc-poc-react.js",
+      "single-spa": "https://cdn.jsdelivr.net/npm/single-spa@5.5.1/lib/system/single-spa.min.js",
+      "react": "https://cdn.jsdelivr.net/npm/react@16.13.1/umd/react.production.min.js",
+      "react-dom": "https://cdn.jsdelivr.net/npm/react-dom@16.13.1/umd/react-dom.production.min.js"
+    }
+  }));
 });
 
 app.listen(process.env.PORT, () =>
